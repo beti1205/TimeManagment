e485473d742a35d3b51543f8c7dc6719adcca66f
beti1205 <beta.bujalska@gmail.com>
2024-06-17 09:57:43 +0200

    fix: Subject text field

--- app/src/main/java/com/example/myapplication/timetracker/presentation/TimeTrackerViewModel.kt
+++ app/src/main/java/com/example/myapplication/timetracker/presentation/TimeTrackerViewModel.kt
@@ -5,14 +5,19 @@ import androidx.lifecycle.viewModelScope
 import com.example.myapplication.timetracker.domain.timetracker.TimeTracker
 import com.example.myapplication.timetracker.domain.usecases.GetAllWorkingSubjectsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@OptIn(FlowPreview::class)
 @HiltViewModel
 class TimeTrackerViewModel @Inject constructor(
     private val timeTracker: TimeTracker,
@@ -23,29 +28,42 @@ class TimeTrackerViewModel @Inject constructor(
 
     private val isSubjectErrorOccurred: MutableStateFlow<Boolean> = MutableStateFlow(false)
 
+    private val workingSubject: MutableStateFlow<String> = MutableStateFlow("")
+
     private val subjects: StateFlow<List<String>> = getAllWorkingSubjectsUseCase()
         .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), emptyList())
 
     val state: StateFlow<TimeTrackerScreenState> = combine(
         timeTrackerState,
         subjects,
-        isSubjectErrorOccurred
-    ) { timeTrackerState, subjects, isSubjectError ->
+        isSubjectErrorOccurred,
+        workingSubject
+    ) { timeTrackerState, subjects, isSubjectError, workingSubject ->
         val filteredSubjects = subjects
             .distinct()
-            .filter { it.contains(timeTrackerState.workingSubject, true) }
+            .filter { it.contains(workingSubject, true) }
 
         TimeTrackerScreenState(
             isActive = timeTrackerState.isActive,
             timeElapsed = timeTrackerState.timeElapsed,
             startTime = timeTrackerState.startTime,
             endTime = timeTrackerState.endTime,
-            workingSubject = timeTrackerState.workingSubject,
+            workingSubject = workingSubject,
             isSubjectErrorOccurred = isSubjectError,
             filteredSubjectList = filteredSubjects
         )
 
     }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(), TimeTrackerScreenState())
+
+    init {
+        viewModelScope.launch {
+            workingSubject
+                .debounce(500)
+                .collectLatest { workingSubject ->
+                    onTimeTrackerWorkingSubjectChanged(workingSubject)
+                }
+        }
+    }
 
     fun toggleTimer() {
         timeTracker.toggleTimer()
@@ -55,8 +73,12 @@ class TimeTrackerViewModel @Inject constructor(
         timeTracker.reset()
     }
 
-    fun onWorkingSubjectChanged(workingSubject: String) {
+    private fun onTimeTrackerWorkingSubjectChanged(workingSubject: String) {
         timeTracker.onWorkingSubjectChanged(workingSubject)
+    }
+
+    fun onWorkingSubjectChanged(workingSubject: String) {
+        this.workingSubject.update { workingSubject }
     }
 
     fun onSubjectErrorChanged(isSubjectErrorOccurred: Boolean) {
